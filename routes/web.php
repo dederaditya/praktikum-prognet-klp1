260d8209c23e35a8a755aabd27c05ffc97615483
John Doe <ardhiyashita2002@gmail.com>
2022-02-18 15:35:35 +0800

    update layout

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/halo', function(){
-    return "Hi Namaku Halo Dunia";
-});
-
-Route::get('/hi', function(){
+Route::get('/', function(){
     return view('layouts.master');
 });
